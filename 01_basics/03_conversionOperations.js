0833da5f17bd71f9a0daa469533f8b82ab943143
Aryan Sharma <114324726+Rad-Monk@users.noreply.github.com>
2024-08-08 06:47:07 +0000

    added about basic operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
 let score = "33.33"
-console.log(typeof score) // string
+// console.log(typeof score) // string
 
 let scoreNumber = Number(score) /* even if the value is not a number, it will still convert it to number,
                                  giving a false value, NaN */
-console.log(typeof scoreNumber) // number
-console.log(scoreNumber) // prints 0 for null and false, 1 for true, and NaN for everything not a number
+// console.log(typeof scoreNumber) // number
+// console.log(scoreNumber) // prints 0 for null and false, 1 for true, and NaN for everything not a number
 // "33" => 33
 // "33abc" => NaN
 // true => 1
@@ -16,8 +16,8 @@ console.log(scoreNumber) // prints 0 for null and false, 1 for true, and NaN for
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn) // boolean
-console.log(booleanIsLoggedIn) // true
+// console.log(typeof booleanIsLoggedIn) // boolean
+// console.log(booleanIsLoggedIn) // true
 
 // 1 => true
 // 0 => false
@@ -30,8 +30,8 @@ console.log(booleanIsLoggedIn) // true
 
 let number = true
 let stringNumber = String(number)
-console.log(typeof stringNumber) // string
-console.log(stringNumber) // "33"
+// console.log(typeof stringNumber) // string
+// console.log(stringNumber) // "33"
 
 // true => "true"
 // false => "false"
@@ -41,3 +41,42 @@ console.log(stringNumber) // "33"
 // null => "null"
 // undefined => "undefined"
 // "" => ""
+
+// ******************************************* O P E R A T I O N S ****************************************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2) // 4
+// console.log(2-2) // 0
+// console.log(2*2) // 4
+// console.log(2/2) // 1
+// console.log(3%2) // 1
+// console.log( 2 ** 3) // 2^3 = 8
+
+let s1 = "Rad "
+let s2 = "and "
+let s3 = "Eve <3"
+// console.log(s1+s2+s3)
+
+// console.log("1" + 2) // 12
+// console.log(1 + "2") // 12
+// console.log(1 + 2) // 3
+// console.log("1" + "2") // 12
+// console.log(2+2+"1") // 41
+// console.log("1"+2+2) // 122
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+// console.log(+null)
+// console.log(+undefined)
+// console.log(+NaN)
+// console.log(+0)
+// console.log(+1)
+
+let gameCounter = 5
+console.log(++gameCounter)
+console.log(gameCounter++)
+console.log(gameCounter)
