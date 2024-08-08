799bee74ca701a5f83fbececec6f585dcb701738
Aryan Sharma <114324726+Rad-Monk@users.noreply.github.com>
2024-08-08 05:23:32 +0000

    added about datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" // this just inflicts stricter regualtions for writing your code, it is recommended to use it
+
+// alert('Hello World!'); // doesn't work in node.js, but works in browser
+
+// Datatypes
+// 1. Number: 2 to power 53 - 1
+// 2. String: 'Hello World!', single double quotes are same
+// 3. Boolean: true or false
+// 4. Undefined: undefined
+// 5. Null: null, means that the variable is empty, null is considered an object
+// 6. Symbol: Symbol('description'), used for unique identifiers
+// 7. BigInt: bigger then number
+// 8. undefined: when you have not entered any value for the variable, consider it as undefined
+
+console.log(typeof null)
+console.log(typeof undefined)
