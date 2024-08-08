8632fc99eb6a13a67fffd3b3a65703b82a535fc2
Aryan Sharma <114324726+Rad-Monk@users.noreply.github.com>
2024-08-08 04:02:15 +0000

    added about variables

--- basics.js
+++ basics.js
@@ -1,3 +1,11 @@
 // commenting code 
 
 console.log('Hello World!'); // this is a single line comment
+
+// Variables
+const id= 1234; // this is a one time declaration variable, which cannot be updated after declaration
+    let name= 'John'; // scoped variable, which can be updated after declaration
+var phn = "1234567890"; // unscoped variable, which can be updated after declaration
+city = "agra" 
+
+console.table([id, name, phn, city])
